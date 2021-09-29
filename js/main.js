52191e5a663105605d35178ec1b0ec379a5c3736
awmills25 <awmills25@gmail.com>
2021-09-29 18:40:28 -0400

    getting data from json and js functions

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
-const axios = require('axios');
+console.log("test");
 
 // Make a request for a user with a given ID
-axios.get('http://api.openweathermap.org/data/2.5/weather?q=Lexington&appid=e11e373f1e129fdd6a2ebaec6434d8b1')
+axios.get('https://api.openweathermap.org/data/2.5/weather?q=Lexington&appid=e11e373f1e129fdd6a2ebaec6434d8b1')
   .then(function (response) {
     // handle success
-    //async?
-    //await?
-    console.log(response);
+    cityValue = response.data.name;
+    tempValue = response.data.main.temp;
+    conditionValue = response.data.weather[0].main;
+    console.log(data);
   })
   .catch(function (error) {
-    if (error != json || error != jsonInt) {
-        errorMsg.innerHTML = "You have entered an invalid zip code. " 
-    }
+    // handle failure
+    console.error(error);
   })
 
-//trying to get a piece of the parsed data into the html
-var json = {"name":"Lexington", "clouds":"{all:20}"};
-var jsonInt = {"temp":"301.13"}
-var stringData = JSON.parse(json);
-var intData = JSON.parse(jsonInt);
-var allData = stringData + intData;
-document.getElementById("temp").innerHTML = Math.round(allData.temp);
-document.getElementById("tempF").innerHTML = Math.round(((allData.temp-273.15)*(9/5)+32));
-document.getElementById("tempC").innerHTML = Math.round((allData.temp-273.15));
+var cityValue;
+var tempValue;
+var tempFValue;
+var tempCValue;
+var conditionValue;
+// //trying to get a piece of the parsed data into the html
+// var json = {"name":"Lexington", "clouds":"{all:20}"};
+// var jsonInt = {"temp":"301.13"}
+// var stringData = JSON.parse(json);
+// var intData = JSON.parse(jsonInt);
+// var allData = stringData + intData;
+// document.getElementById("temp").innerHTML = Math.round(allData.temp);
+// document.getElementById("tempF").innerHTML = Math.round(((allData.temp-273.15)*(9/5)+32));
+// document.getElementById("tempC").innerHTML = Math.round((allData.temp-273.15));
 
 //create variables
 var goButton = document.getElementById("goButton");
@@ -36,13 +41,14 @@ var errorMsg = document.getElementById("error");
 var dataBox = document.getElementById("dataBox");
 var zip = document.getElementById("zip");
 
-//weather state object
-var dataBox = {
+// //weather state object
+// var dataBox = {
     
-}
+// }
 
 //function that sends us to the next page and runs state change
 function goButtonMethod() {
+    getWeather();
     onStateChange();
 }
 //on click event listeners for each button sending to next page
@@ -58,21 +64,24 @@ function show(element) {
     element.style.visibility = "visible";
 }
 
-//function that runs and pulls up when the page is loaded
-function init(){
-    onStateChange();
-}
-
 //state management function
 function onStateChange() {
-    //if zip is valid
-    if (zip) {
-        show(dataBox);
-        hide(errorMsg);
+    show(dataBox);
+    hide(errorMsg);
+}
+
+//same thing as what is above but with different syntax
+async function getWeather() {
+    try{
+        var response = await axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40504&appid=e11e373f1e129fdd6a2ebaec6434d8b1');
+        // handle success
+        cityValue = response.data.name;
+        tempValue = response.data.main.temp;
+        conditionValue = response.data.weather[0].main;
+        console.log(data);
     }
-    //if zip isn't valid
-    else {
-        show(errorMsg);
-        hide(dataBox);
+    catch(error) {
+        console.error(error);
     }
+    console.log(response);
 }
