d1506c18acd513f918a5a870966aa761cfeadd72
awmills25 <awmills25@gmail.com>
2021-09-30 10:39:26 -0400

    getting things to display on the page

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-console.log("test");
-
-// Make a request for a user with a given ID
-axios.get('https://api.openweathermap.org/data/2.5/weather?q=Lexington&appid=e11e373f1e129fdd6a2ebaec6434d8b1')
-  .then(function (response) {
-    // handle success
-    cityValue = response.data.name;
-    tempValue = response.data.main.temp;
-    conditionValue = response.data.weather[0].main;
-    console.log(data);
-  })
-  .catch(function (error) {
-    // handle failure
-    console.error(error);
-  })
-
 var cityValue;
 var tempValue;
 var tempFValue;
 var tempCValue;
 var conditionValue;
-// //trying to get a piece of the parsed data into the html
-// var json = {"name":"Lexington", "clouds":"{all:20}"};
-// var jsonInt = {"temp":"301.13"}
-// var stringData = JSON.parse(json);
-// var intData = JSON.parse(jsonInt);
-// var allData = stringData + intData;
-// document.getElementById("temp").innerHTML = Math.round(allData.temp);
-// document.getElementById("tempF").innerHTML = Math.round(((allData.temp-273.15)*(9/5)+32));
-// document.getElementById("tempC").innerHTML = Math.round((allData.temp-273.15));
+
+// Make a request for a user with a given ID
+function getWeather() {
+    axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40509&appid=e11e373f1e129fdd6a2ebaec6434d8b1')
+    .then(function (response) {
+        // handle success
+        cityValue = response.data.name;
+        tempValue = Math.round(response.data.main.temp);
+        tempFValue = Math.round((tempValue-273.15)*(9/5)+32);
+        tempCValue = Math.round((tempValue-273.15));
+        conditionValue = response.data.weather[0].description;
+
+        city.innerHTML=cityValue;
+        temp.innerHTML=tempValue + "°K";
+        tempF.innerHTML=tempFValue + "°F";
+        tempC.innerHTML=tempCValue + "°C";
+        condition.innerHTML=conditionValue;
+        addImage(tempFValue);
+        //console.log(data);
+    })
+    .catch(function (error) {
+        // handle failure
+        console.error(error);
+        //show(errorMsg);
+    })
+}
 
 //create variables
 var goButton = document.getElementById("goButton");
@@ -41,18 +42,19 @@ var errorMsg = document.getElementById("error");
 var dataBox = document.getElementById("dataBox");
 var zip = document.getElementById("zip");
 
-// //weather state object
-// var dataBox = {
-    
-// }
-
 //function that sends us to the next page and runs state change
 function goButtonMethod() {
-    getWeather();
-    onStateChange();
-}
+        getWeather();
+        onStateChange();
+    }
 //on click event listeners for each button sending to next page
 goButton.addEventListener('click', goButtonMethod);
+
+//state management function
+function onStateChange() {
+    show(dataBox);
+    hide(errorMsg);
+}
 
 //function to hide elements
 function hide(element) {
@@ -64,24 +66,30 @@ function show(element) {
     element.style.visibility = "visible";
 }
 
-//state management function
-function onStateChange() {
-    show(dataBox);
-    hide(errorMsg);
+function addImage(temp) {
+    if (temp > 70) {
+        //document.getElementById("img").src="./img/hot.jpg";
+        //img.innerHTML = "./img/hot.jpg";
+    } else if (temp <= 70 && temp > 32){
+        //img.innerHTML = "./img/mild.jpg";
+    } else{
+        //img.innerHTML = "./img/cold.jpg";
+    }
 }
 
-//same thing as what is above but with different syntax
-async function getWeather() {
-    try{
-        var response = await axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40504&appid=e11e373f1e129fdd6a2ebaec6434d8b1');
-        // handle success
-        cityValue = response.data.name;
-        tempValue = response.data.main.temp;
-        conditionValue = response.data.weather[0].main;
-        console.log(data);
-    }
-    catch(error) {
-        console.error(error);
-    }
-    console.log(response);
-}
+
+//   //same thing as what is above but with different syntax
+//   function getWeather() {
+//       try{
+//           var response = await axios.get('https://api.openweathermap.org/data/2.5/weather?zip=40504&appid=e11e373f1e129fdd6a2ebaec6434d8b1');
+//           // handle success
+//           cityValue = response.data.name;
+//           tempValue = response.data.main.temp;
+//           conditionValue = response.data.weather[0].main;
+//           console.log(data);
+//       }
+//       catch(error) {
+//           console.error(error);
+//       }
+//       console.log(response);
+//   }
